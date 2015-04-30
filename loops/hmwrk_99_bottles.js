6f8c9f2da9d325c14592f9d6874c653664efaae5
LindsayElia <lindsay.elia@gmail.com>
2015-04-30 02:05:25 -0700

    completed 99 bottles problem

--- loops/hmwrk_99_bottles.js
+++ loops/hmwrk_99_bottles.js
@@ -18,3 +18,42 @@ iterate down until 0
 if value === 1 then print 'bottle'
 else print 'bottles'
 */
+
+/* song lyrics:
+
+99 bottles of beer on the wall,
+99 bottles of beer...
+take one down, pass it around, 98 bottles of beer on the wall!
+
+repeat
+*/
+
+var numBottles = 99;
+var pluralBottles = "bottles";
+var singularBottles = "bottle";
+
+while (numBottles >=0){
+	
+	var oneLessBottle = (numBottles - 1);
+	
+	// condition to determine bottle type when it is mentioned first two times in song
+	if (numBottles === 1){
+		var bottleTypeFirstLine = singularBottles;
+	}
+	else var bottleTypeFirstLine = pluralBottles;
+
+	
+	// condition to determine bottle type when it is mentioned the third time in song
+	if (oneLessBottle === 1){
+		var bottleTypeSecondLine = singularBottles;
+	}
+	else
+		var bottleTypeSecondLine = pluralBottles;
+		
+	console.log(numBottles + " " + bottleTypeFirstLine + " of beer on the wall, " + numBottles + " " + bottleTypeFirstLine + " of beer...take one down, pass it around, " + oneLessBottle + " " + bottleTypeSecondLine + " of beer on the wall!");
+
+	numBottles = numBottles - 1;
+
+}
+
+
