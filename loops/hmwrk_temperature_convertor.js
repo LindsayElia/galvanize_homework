cb71559097b538d8fd545f2fe669ef04f27ba3d5
LindsayElia <lindsay.elia@gmail.com>
2015-04-30 01:42:39 -0700

    completed temperature convertor with bonus

--- loops/hmwrk_temperature_convertor.js
+++ loops/hmwrk_temperature_convertor.js
@@ -10,14 +10,15 @@ Bonus: ask the user first if they want to convert from F to C or C to F.
 My logic in English...
 
 Ask the user for a value
-convert the value from a string to a number
+convert the value from a string to a number -- possibly unecessary with JavaScript due
+to type conversion?
 do some math wizardry to convert the number from Celsius to Fahrenheit
 convert the value back from a number to a string -- possibly unecessary with JavaScript due
 to type conversion?
 display the new value
 
 Bonus
-as if the user's value is Celsius or Fahrenheit
+ask if the user's value is Celsius or Fahrenheit
 get value
 if Celsius, follow the logic outlined above
 if Farenheit, do a different set of steps (same logic as above,
@@ -25,3 +26,30 @@ except the math wizardry converts F to C)
 else, tell the user to choose c or f!
 display the old value and the new value and the scale for each one
 */
+
+
+//Celsius to Fernheit:
+
+var temperature = prompt("Please give me the temperature in Celsius.", " ");
+
+temperature = ((temperature * 9)/5)+32;
+
+
+//Bonus Answer:
+
+var type = prompt("Would you like to start with Celsius or Fahrenheit? Type \"C\" for Celsius and \"F\" for Fahrenheit.");
+
+if (type === "C"){
+	var temperatureC = prompt("Please give me the temperature in Celsius.", " ");
+	temperature = ((temperatureC * 9)/5)+32;
+	prompt("With a temperature of " + temperatureC + " in Celsius, that converts to " + temperature + " in Fahrenheit.");
+}
+else if (type === "F"){
+	var temperatureF = prompt("Please give me the temperature in Fahrenheit.", " ");
+	temperature = (((temperatureF - 32)* 5)/9);
+	prompt("With a temperature of " + temperatureF + " in Fahrenheit, that converts to " + temperature + " in Celsius.");
+}
+else {
+	prompt("Sorry, that was not a capital C or capital F. You will need to run this program again.");
+}
+
