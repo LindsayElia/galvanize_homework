29cd92be2b213466e57e875bc08752d697b53221
LindsayElia <lindsay.elia@gmail.com>
2015-05-01 06:55:45 -0700

    complete bean counting hmwrk

---
+++ functions/hmwrk_bean_counting.js
@@ -0,0 +1,65 @@
+/* 
+Eloquent JavaScript, Chapter 3, Exercise: Bean Counting
+
+Instructions:
+Write a function countBs that takes a string as its only argument and 
+returns a number that indicates how many uppercase “B” characters are in the string.
+
+Next, write a function called countChar that behaves like countBs, 
+except it takes a second argument that indicates the character that is to be 
+counted (rather than counting only uppercase “B” characters). 
+
+Rewrite countBs to make use of this new function.
+
+FOR EXAMPLE:
+// Your code here.
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
+
+*/
+
+/*
+My logic in English...
+
+declare a function with the name 'countBs'
+pass into it one argument (x)
+assign the argument to a var called stringToEvaluate
+
+use .charAt(N) to look at each letter in the string
+use a for loop to cycle through all of the letters one at a time,
+until I reach the .length of the string
+use variable 'i' to hold each letter as I loop through them
+create a variable called count to hold the count of the "B"s
+
+use if else to decide if the letter is "B"
+if it is, add it to the count of letters, then repeat the for loop
+else, do nothing & repeat the for loop
+
+create a variable called count to hold the count of the "B"s
+
+use console.log to show the returned result
+*/
+
+var countBs = function(x) {
+	var stringToEvaluate = x;
+	// var letter = 0;
+	var count = 0;
+	for (i = 0; i < stringToEvaluate.length; i++) {
+		if (stringToEvaluate.charAt(i) === "B"){
+			count = count + 1;
+		}
+		else {
+					// do nothing
+		}
+	}
+	return count;
+};
+
+console.log(countBs("BigBlueBaseballBat"));
+
+
+
+
