65e12cba45df24d2b260ba8251419378c3130cde
LindsayElia <lindsay.elia@gmail.com>
2015-05-01 07:22:37 -0700

    really completed bean counting exercise this time. forgot about making second function. oops.

--- functions/hmwrk_bean_counting.js
+++ functions/hmwrk_bean_counting.js
@@ -43,6 +43,8 @@ create a variable called count to hold the count of the "B"s
 use console.log to show the returned result
 */
 
+
+// ORIGINAL countBs FUNCTION
 var countBs = function(x) {
 	var stringToEvaluate = x;
 	// var letter = 0;
@@ -59,6 +61,48 @@ var countBs = function(x) {
 };
 
 console.log(countBs("BigBlueBaseballBat"));
+
+
+// NEW 'countChar' FUNCTION
+
+/*
+compare letter to se
+function takes two string arguments
+compare letters in first string to the second argument
+if true, add 1 to the count
+else, do nothing
+loop until all strings have been evaluated
+*/
+
+var countChar = function(x,y) {
+	var stringToEvaluate = x;
+	var letterToEvaluate = y;
+	var count = 0;
+	for (i = 0; i < stringToEvaluate.length; i++) {
+		if (stringToEvaluate.charAt(i) === letterToEvaluate){
+			count = count + 1;
+		}
+		else {
+					// do nothing
+		}
+	}
+	return count;
+};
+
+console.log(countChar("rosyredraya", "r"));
+
+
+// NEW 'countBs' FUNCTION USING 'countChar' FUNCTION
+function countBs () {
+	return countChar ();
+}
+
+console.log(countBs("BigBlueBaseballBatBBBB", "B"));
+
+
+
+
+
 
 
 
