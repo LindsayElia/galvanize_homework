8665a221756e7b6e58bf1c73330b426f6763e7c9
LindsayElia <lindsay.elia@gmail.com>
2015-06-03 22:51:44 -0700

    edited comments

--- 06-03_node_exercises/newLineCountv2.js
+++ 06-03_node_exercises/newLineCountv2.js
@@ -9,10 +9,12 @@ var numLines;
 
 // Idiomatic Node.js callbacks normally follow this pattern: 
 // function callback (err, data) { /* ... */ }
+// I also tried this with "utf-8" as the second parameter into readFile instead of using
+// .toString() inside of the callback, but neither are working
 fsModule.readFile("test.txt", function(err, data){
 	if (err) throw err // not sure why we do not need brackets {} or a semi-colon ; after the condition here??
 	contents = ("test.txt").toString();
-	numLines = contents.split(/\r\n|\r|\n/).length - 1;
+	numLines = contents.split(/\r\n|\r|\n/).length - 1;	// perhaps this won't work for this format?
 	console.log(numLines.toString());
 });
 
@@ -20,11 +22,4 @@ fsModule.readFile("test.txt", function(err, data){
 // use process.argv[2] to pass in the file name via the command line
 // process.argv is an array, and the element at the [2] index will be the first thing that we type after 
 // 'node' and 'thisfilename.js'
-
-
-// .length gives me the total number of lines, so I want one less because I only want the number of returns, not lines
-
-
-// log the result in the command line, converted to a string so humans can read it
-// instead of as a the Buffered object thingy
 
